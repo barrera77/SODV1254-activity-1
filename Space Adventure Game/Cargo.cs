35b454cee41b966f15879c5b9d0351f3c8cfabf4
barrera77 <barrera_ml@hotmail.com>
2025-01-19 01:18:11 -0700

    Initial commit

---
+++ Space Adventure Game/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Adventure_Game
+{
+    public class Cargo
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }  
+
+        //Constructor
+        public Cargo(string name, double weight) 
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+    }
+}
---
+++ Space Adventure Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Space_Adventure_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

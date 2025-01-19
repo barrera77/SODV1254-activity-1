c4cee9b6ac97806a6153ecb856fb1ce36d08eae0
barrera77 <barrera_ml@hotmail.com>
2025-01-19 02:13:09 -0700

    Added logic to Classes

--- Space Adventure Game/Planet.cs
+++ Space Adventure Game/Planet.cs
@@ -8,5 +8,65 @@ namespace Space_Adventure_Game
 {
     internal class Planet
     {
+        public string Name { get; set; }
+        public List<Cargo> AvailableCargo { get; private set; }
+        public bool RefuelingStation { get; set; }
+
+        //Constructor
+        public Planet(string name, bool refuelingStation)
+        {
+            Name = name;
+            RefuelingStation = refuelingStation;
+            AvailableCargo = new List<Cargo>();
+        }
+
+        public void AddCargo(Cargo item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Cargo item cannot be null");
+            }
+            else
+            {
+                AvailableCargo.Add(item);
+            }
+        }
+
+        public void RemoveCargo(Cargo item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Cargo item cannot be null");
+            }
+            if (!AvailableCargo.Contains(item))
+            {
+                throw new InvalidOperationException("Item Doenst Exist");
+            }
+            else
+            {
+                AvailableCargo.Remove(item);
+            }
+        }
+
+        public void RefuelSpacehip(SpaceShip spaceShip, double amount)
+        {
+            if (spaceShip == null)
+            {
+                throw new ArgumentNullException(nameof(spaceShip), "Spaceship cannot be null");
+            }
+            if (!RefuelingStation)
+            {
+                throw new InvalidOperationException($"{Name} is not a refueling station");
+            }
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Fuel amount must be greater than 0");
+            }
+            else
+            {
+                spaceShip.Fuel += amount;
+            }
+
+        }
     }
 }
--- Space Adventure Game/SpaceShip.cs
+++ Space Adventure Game/SpaceShip.cs
@@ -8,5 +8,93 @@ namespace Space_Adventure_Game
 {
     internal class SpaceShip
     {
+        public string Name { get; set; }
+        public double Fuel {  get; set; }
+        public double CargoCapacity { get; private set; }
+        public List<Cargo> CargoList { get; private set; }
+        public Planet Location { get; private set; }
+
+        public void Fly(Planet destination)
+        {
+            if(destination == null)
+            {
+                throw new ArgumentNullException(nameof(destination), "Destination cannot be null");
+            }
+            if(Fuel <= 0)
+            {
+                throw new InvalidOperationException("Not enough fuel for the flight");
+            }
+
+            Location = destination;
+        }
+
+        public void Refuel(double amount)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Fuel amount must be greater than 0");
+            }
+            else
+            {
+                Fuel += amount;
+            }
+        }
+
+        public void LoadCargo(Cargo item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Cargo item cannot be null");
+            }
+
+            double currentCargoWeight = GetTotalCargoWeight();
+
+            if (currentCargoWeight + item.Weight > CargoCapacity)
+            {
+                throw new InvalidOperationException("Not enough cargo capacity to add this item to the load");
+            }
+            else
+            {
+                CargoList.Add(item);
+            }
+        }
+
+        public void UnloadCargo(Cargo item)
+        {
+            if(item ==null)
+            {
+                throw new ArgumentNullException(nameof(item), "Cargo item cannot be null");
+            }
+            if(!CargoList.Contains(item))
+            {
+                throw new InvalidOperationException("Item Doenst Exist");
+            }
+            else
+            {
+                CargoList.Remove(item);                
+            }
+        }
+
+        //Constructor
+        public SpaceShip(string name, double fuel, double cargoCapacity, Planet initialLocation)
+        {
+            Name = name;
+            Fuel = fuel;
+            CargoCapacity = cargoCapacity;
+            CargoList = new List<Cargo>();
+            Location = initialLocation;
+        }
+
+        //Helper methods
+        private double GetTotalCargoWeight()
+        {
+            double totalCargoWeight = 0;
+
+            foreach(var item in CargoList)
+            {
+                totalCargoWeight += item.Weight;    
+            }
+            return totalCargoWeight;
+        }
     }
 }
